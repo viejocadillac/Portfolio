804e53d45ea4fbc523c5ca6e6da62d9469f64d36
Mathias Moreira <viejocadillac@gmail.com>
2020-09-24 00:00:30 -0300

    Se modifican los inputs de contacto

--- src/sections/Contacto.jsx
+++ src/sections/Contacto.jsx
@@ -1,67 +1,100 @@
 import React from 'react';
 import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Section from '../components/Section';
-import Container from '../components/Container'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Container from '../components/Container';
+
+
+
+
 
 const Contacto = ({ id, className, referencia, show }) => {
 
-    const formulario = React.useRef()
+  const formulario = React.useRef()
 
-    const sendForm = (event)=> {
-        event.preventDefault();
-        const data = new FormData(formulario.current);
-     
-
-        fetch("https://usebasin.com/f/8cbe29ba9b2a", {
-            method: 'POST',
-            body: data,
-        }).then((response) => {
-            console.log(response)
-        })
-    }
+  const sendForm = (event) => {
+    event.preventDefault();
+    const data = new FormData(formulario.current);
 
 
+    fetch("https://usebasin.com/f/8cbe29ba9b2a", {
+      method: 'POST',
+      body: data,
+    }).then((response) => {
+      console.log(response)
+    })
+  }
 
 
-    return (
-        <Section ref={referencia} id={id} className={className}>
-            <Section.Title text={['Con', 'tacto.']} />
-            <Container>
-                <Section.Body className="proyectos-container" >
-
-                    <div className="g-flex">
-
-                        <div>
-                            <p>Enviame un mensaje, o contactame por las redes!</p>
-                            <div className="icons-container">
-                                <a href="https://github.com/viejocadillac"><FontAwesomeIcon icon={['fab', 'github']} size="2x" /></a>
-                                <a href="https://www.linkedin.com/in/mathias-moreira-b77290195/"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" /></a>
-                             
-                            </div>
-
-                        </div>
-
-                        <form ref={formulario} className="" onSubmit={sendForm}>
-                            <label for="nombre">Tu nombre</label>
-                            <input id="nombre" name="nombre" type="text"></input>
-
-                            <label for="email">Tu email</label>
-                            <input id="email" name="email" type="email"></input>
-
-                            <label for="email">Mensaje</label>
-                            <textarea id="mensaje" name="mensaje" ></textarea>
-                            <button type="submit" >Enviar</button>
-                        </form>
 
 
-                    </div>
+  return (
+    <Section ref={referencia} id={id} className={className}>
+      <Section.Title text={['Con', 'tacto.']} />
+      <Container>
+        <Section.Body className="proyectos-container" >
 
-                </Section.Body>
-            </Container>
-        </Section>
+          <div className="g-flex">
 
-    )
+            <div>
+              <p>Enviame un mensaje, o contactame por las redes!</p>
+              <div className="icons-container">
+                <a href="https://github.com/viejocadillac"><FontAwesomeIcon icon={['fab', 'github']} size="2x" /></a>
+                <a href="https://www.linkedin.com/in/mathias-moreira-b77290195/"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" /></a>
+
+              </div>
+
+            </div>
+
+            <form ref={formulario} className="" onSubmit={sendForm}>
+              <Input
+                id="nombre"
+                margin="normal"
+                name="nombre"
+                size="small"
+                variant="filled"
+                label="Nombre"
+                type="text"
+                classes={{ root: 'input-root' }}
+                required
+              />
+              <Input
+                id="email"
+                margin="normal"
+                name="email"
+                size="small"
+                variant="filled"
+                label="Email"
+                type="email"
+                classes={{ root: 'input-root' }}
+                required
+              />
+              <Input
+                id="mensaje"
+                margin="normal"
+                name="mensaje"
+                size="small"
+                variant="filled"
+                label="Mensaje"
+                multiline
+                rows="4"
+                classes={{ root: 'input-root' }}
+                required
+              />
+              <Button type="submit" variant="contained" color="primary">Enviar</Button>
+            </form>
+
+
+          </div>
+
+        </Section.Body>
+      </Container>
+    </Section>
+
+  )
 }
 
 export default styled(Contacto)`
@@ -73,13 +106,20 @@ a {
 }
 p {
     text-align:center;
+    color: ${({ theme }) => theme.colors.navBarText};
+}
+
+.input-root {
+  background-color: white;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
 }
 
 .icons-container{
     display: flex;
     justify-content: center;
     margin-bottom: 3rem;
-    color: ${({theme})=> theme.colors.accent};
+    color: ${({ theme }) => theme.colors.accent};
 
     * {
         margin: 0.5rem;
@@ -91,33 +131,19 @@ p {
         flex-direction: column;
         max-width: 400px;
         margin: 0 auto;
+        font-family: 'Raleway', sans-serif,Arial, Helvetica, sans-serif;
+        color: ${({ theme }) => theme.colors.navBarText};
+
+      
     }
 
-    input, textarea {
-        box-sizing: border-box;
-        border: 1px solid ${({ theme }) => theme.colors.otherThemeBackground};
-        height: 2em;
-        border-radius: 1em;
-        margin-bottom: 1.5rem;
-        padding: 0.8em;
-        &:focus {
-          
-            outline: none;
-            border: 2px solid ${({ theme }) => theme.colors.otherThemeBackground};
 
-
-        }
-    }
-    textarea {
-        height: auto;
-    }
 
     button {
-        max-width: 7em;
-        padding: 0.6em 1.2em;
+  
         align-self: flex-end;
-        border-radius: 3.8em;
-        border:none;
+        border-radius: 3px;
+        
         color: ${({ theme }) => theme.colors.claro};
 
         background-color: ${({ theme }) => theme.colors.accent};
