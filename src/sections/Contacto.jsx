1cb97179be43eda318a872754b9373e4cb11aae6
Mathias Moreira <viejocadillac@gmail.com>
2020-09-28 17:38:06 -0300

    Estilo

--- src/sections/Contacto.jsx
+++ src/sections/Contacto.jsx
@@ -1,55 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-import { makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Section from '../components/Section';
 import Container from '../components/Container';
 
-
-
-
-
-const Contacto = ({ id, className, referencia, show }) => {
-
-  const formulario = React.useRef()
-
+const Contacto = ({ id, className, referencia, show, }) => {
+  const formulario = React.useRef();
   const sendForm = (event) => {
     event.preventDefault();
     const data = new FormData(formulario.current);
 
-
-    fetch("https://usebasin.com/f/8cbe29ba9b2a", {
+    fetch('https://usebasin.com/f/8cbe29ba9b2a', {
       method: 'POST',
       body: data,
     }).then((response) => {
-      console.log(response)
-    })
-  }
-
-
-
+      console.log(response);
+    });
+  };
 
   return (
     <Section ref={referencia} id={id} className={className}>
       <Section.Title text={['Con', 'tacto.']} />
       <Container>
-        <Section.Body className="proyectos-container" >
+        <Section.Body className="proyectos-container">
 
           <div className="g-flex">
 
             <div>
               <p>Enviame un mensaje, o contactame por las redes!</p>
               <div className="icons-container">
-                <a href="https://github.com/viejocadillac"><FontAwesomeIcon icon={['fab', 'github']} size="2x" /></a>
-                <a href="https://www.linkedin.com/in/mathias-moreira-b77290195/"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" /></a>
-
+                <a href="https://github.com/viejocadillac">
+                  <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+                </a>
+                <a href="https://www.linkedin.com/in/mathias-moreira-b77290195/">
+                  <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+                </a>
               </div>
-
             </div>
 
-            <form ref={formulario} className="" onSubmit={sendForm}>
+            <form ref={formulario} onSubmit={sendForm}>
               <Input
                 id="nombre"
                 margin="normal"
@@ -86,36 +77,34 @@ const Contacto = ({ id, className, referencia, show }) => {
               />
               <Button type="submit" variant="contained" color="primary">Enviar</Button>
             </form>
-
-
           </div>
-
         </Section.Body>
       </Container>
     </Section>
-
-  )
-}
+  );
+};
 
 export default styled(Contacto)`
-display:flex;
-margin-bottom: 6rem;
-flex-direction:column;
-a {
-    color: inherit;
-}
-p {
-    text-align:center;
-    color: ${({ theme }) => theme.colors.navBarText};
-}
+  display:flex;
+  margin-bottom: 6rem;
+  flex-direction:column;
 
-.input-root {
-  background-color: white;
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-}
+  a {
+      color: inherit;
+  }
 
-.icons-container{
+  p {
+      text-align:center;
+      color: ${({ theme }) => theme.colors.navBarText};
+  }
+
+  .input-root {
+    background-color: white;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+  }
+
+  .icons-container{
     display: flex;
     justify-content: center;
     margin-bottom: 3rem;
@@ -124,35 +113,21 @@ p {
     * {
         margin: 0.5rem;
     }
-}
+  }
 
-    form {
-        display: flex;
-        flex-direction: column;
-        max-width: 400px;
-        margin: 0 auto;
-        font-family: 'Raleway', sans-serif,Arial, Helvetica, sans-serif;
-        color: ${({ theme }) => theme.colors.navBarText};
+  form {
+      display: flex;
+      flex-direction: column;
+      max-width: 400px;
+      margin: 0 auto;
+      font-family: 'Raleway', sans-serif,Arial, Helvetica, sans-serif;
+      color: ${({ theme }) => theme.colors.navBarText};
+  }
 
-      
-    }
-
-
-
-    button {
-  
-        align-self: flex-end;
-        border-radius: 3px;
-        
-        color: ${({ theme }) => theme.colors.claro};
-
-        background-color: ${({ theme }) => theme.colors.accent};
-
-    }
-
-  
-
-
-
-
-`;
+  button {
+    align-self: flex-end;
+    border-radius: 3px;
+    color: ${({ theme }) => theme.colors.claro};
+    background-color: ${({ theme }) => theme.colors.accent};
+  }
+  `;
