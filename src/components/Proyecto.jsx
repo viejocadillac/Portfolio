50ed07772224378cbbf4da1ff21b21dabf05c159
Mathias Moreira <viejocadillac@gmail.com>
2020-09-28 18:04:05 -0300

    Se modifica cada proyecto

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -1,103 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
-import Pulse from 'react-reveal/Pulse';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Proyecto = ({className, nombre, descripcion, githubLink, liveUrl}) => {
-    return (
-     
-        <article className={className}>
-            <header>
-                <h1>{nombre.replace(/-/g, ' ')}</h1>
-                <p>{descripcion}</p>
-            </header>
-            <footer>
-                <a href={githubLink} className="github"><FontAwesomeIcon icon={['fab', 'github']} size="2x"/>Github</a>
-                { liveUrl ? <a href={liveUrl} className="live">Live</a> : <span className="live">Live</span>}
-            </footer>
-        
-        </article>
+const Proyecto = ({
+  className, nombre, imgSrc, githubLink, liveUrl, area,
+}) => {
+  const ref = React.useRef();
 
+  return (
 
+    <article className={className}>
 
+      <img className="imagen" src={`${process.env.PUBLIC_URL}${imgSrc}`} alt="imagen de ejemplo" />
+      <h3 ref={ref} name={nombre} className="nombre">{nombre}</h3>
 
-        
-    )
-}
+      <footer>
+        <a href={githubLink} className="github">
+          <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+          Github
+        </a>
+        { liveUrl ? <a href={liveUrl} className="live">Live</a> : <span className="live">Live</span>}
+      </footer>
+
+    </article>
+  );
+};
 
 export default styled(Proyecto)`
-box-shadow: 2px 5px 7px -2px rgba(0,0,0,0.4);
-text-align:center;
-padding: 1rem ;
+grid-area: ${({ area }) => area};
 display: flex;
-flex-direction: column;
-header {
+position: relative;
+
+.imagen {
+    width: 100%;
+    object-fit: cover;
+}
+
+.nombre {
+    position: absolute;
     margin: 0;
-    color: ${({theme})=> theme.colors.claro};
-    padding: 2rem;
-    min-height: 150px;
-    flex-grow: 1;
-    box-sizing: border-box;
-    background-color: ${({theme})=>theme.colors.primario};
+    top: 0;
+    left: 0;
+    color: white;
+    font-size: 1.3em;
+    font-weight: 600;
+    padding: 1rem;
+    opacity: 0.8;
 }
-
-h1 {
-    font-size: 1.5em;
-}
-
- p {
-     opacity: 0.6;
- }
 
 footer {
-    padding-top: 1rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    color: ${({theme})=> theme.colors.accent};
+  position: absolute;
+  width: 100%;
+  padding: 0 1rem;
+  box-sizing: border-box;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${({ theme }) => theme.colors.accent};
 
-    a {
+  a {
     color: inherit;
     text-decoration: none;
+  }
 
- 
-}
+  .live {
+    font-weight: 700;
+    color: ${({ theme, liveUrl }) => (liveUrl ? theme.colors.accent : 'grey')};
+    opacity: ${({ liveUrl }) => !liveUrl && 0.5};
+  }
 
-    .live {
-        font-weight: 700;
-     
-        color: ${({theme, liveUrl}) => liveUrl ? theme.colors.accent : 'grey'};
-        opacity: ${({liveUrl}) => !liveUrl && 0.5};
-
-
-
-
+  .github {
+    font-size: 0.8em;
+    display: flex;
+    align-items: center;
+      * {
+      margin: 0.2em;
     }
-
-    .github {
-        font-size: 0.8em;
-        display: flex;
-        align-items: center;
-         * {
-             margin: 0.2em;
-         }
-
-    }
+  }
 }
-
-h1 {
-    margin: 0;
-
-}
-   
-   
-  
-
-
- 
- 
- 
-
-
-
-`
+`;
