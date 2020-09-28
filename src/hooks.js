a84ad589d44de025a2fd7c8386235e631ddb3455
Mathias Moreira <viejocadillac@gmail.com>
2020-09-28 18:13:43 -0300

    Estilo y se elimina funcion innecesaria

--- src/hooks.js
+++ src/hooks.js
@@ -1,108 +1,83 @@
-import { useLayoutEffect, useEffect, useState, useRef } from 'react';
+import {
+  useLayoutEffect, useEffect, useState, useRef,
+} from 'react';
 import { setThemeCookie, parseCookies } from './utils';
 
-
 /**
- * 
+ *
  * @param {function} callback Funcion llamada cuando se hace scroll
  */
 const useScroll = (callback) => {
-
-  const onScroll = (event) => {
-      // Cross browser support.
-      const scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-
-      window.requestAnimationFrame(() => callback(scrollPos))
-     
-  }
+  const onScroll = () => {
+    // Cross browser support.
+    const scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop;
+    window.requestAnimationFrame(() => callback(scrollPos));
+  };
 
   useEffect(() => {
-      document.addEventListener('scroll', onScroll);
-      return () => {
-          document.removeEventListener('scroll', onScroll);
-      }
-  })
+    document.addEventListener('scroll', onScroll);
+    return () => {
+      document.removeEventListener('scroll', onScroll);
+    };
+  });
 };
 
-const useTheme = (theme1, theme2 ) => {
-  const [theme, setTheme] = useState(theme1) // Number 1 default theme
+// TODO Permitir que se pase una lista de temas
+const useTheme = (theme1, theme2) => {
+  const [theme, setTheme] = useState(theme1); // Number 1 default theme
 
   const toggleTheme = () => {
     const newTheme = theme === theme1 ? theme2 : theme1;
+    // FIXME Cambiar el guardado en cookies por local storage
     setThemeCookie(newTheme.name);
     setTheme(newTheme);
-  }
+  };
 
   useLayoutEffect(() => {
-    const cookies = parseCookies()
+    const cookies = parseCookies();
     if (cookies.modo === theme2.name) {
       setTheme(theme2);
-    } 
+    }
   }, [setTheme]);
 
   return [theme, toggleTheme];
-}
+};
 
 const useToggleShowOn = (pixels, initialShow) => {
-  const [show, setShow] = useState(initialShow)
-  
+  const [show, setShow] = useState(initialShow);
   useScroll((scrollPos) => {
     if (scrollPos > pixels && !show) {
-      setShow(true)
+      setShow(true);
     }
 
     if (scrollPos < pixels && show) {
-      setShow(false)
+      setShow(false);
     }
-
-  })
-  return show
-}
-
-
+  });
+  return show;
+};
 
 const useIntersectionObserver = (options) => {
-    const [elements, setElements] = useState([])
-    const [entries, setEntries] = useState()
+  const [elements, setElements] = useState([]);
+  const [entries, setEntries] = useState();
 
-    const callback = (_entries) => {
-        setEntries(_entries)
-    }
+  const callback = (_entries) => {
+    setEntries(_entries);
+  };
 
-    const observer = useRef(new IntersectionObserver(callback, {threshold: 0.35}))
+  const observer = useRef(new IntersectionObserver(callback, options));
 
-    useEffect(()=> {
-      elements.forEach( (element) => observer.current.observe(element) )
-        return () => observer.current.disconnect()
-    }, [elements])
+  useEffect(() => {
+    elements.forEach((element) => observer.current.observe(element));
+    return () => observer.current.disconnect();
+  }, [elements]);
 
-    return [setElements, entries]
-}
-
-const useOtherIntersection = (toObserve, navRef, callback) => {
-
-  const [entries, setEntries] = useState()
-  console.log(`navRef en useIntersectionObserver: ${navRef}`)
-
-  
-  const observer = useRef(new IntersectionObserver(callback, {threshold: 0.35}))
-
-
-  useEffect(()=> {
-      observer.current.observe(toObserve.current)
-      return () => observer.current.disconnect()
-  }, [toObserve])
-
-  return [entries]
-
-
-
-}
+  return [setElements, entries];
+};
 
 export {
   useScroll,
   useTheme,
   useToggleShowOn,
   useIntersectionObserver,
-  useOtherIntersection
-}
+};
