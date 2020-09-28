5c9334b4d5cb69f334332afa62856b13855c1621
Mathias Moreira <viejocadillac@gmail.com>
2020-09-28 17:46:45 -0300

    Estilo

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,51 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import Zoom from 'react-reveal/Zoom';
-import {useIntersectionObserver, useOtherIntersection} from '../hooks'
 
+const Section = styled(React.forwardRef((props, ref) => (
+  <section id={props.id} ref={ref} className={props.className}>
+    {props.children}
+  </section>
 
-const Section = styled(React.forwardRef( (props, ref) => {
-    return (
-        <section id={props.id} ref={ref} className={props.className} >
-            {props.children}
-        </section>
-
-    )
-
-}))`
+)))`
     min-height: 100vh;
-    background-color: ${({backgroundColor}) => backgroundColor};
-    padding-top: 7rem;
-    
-`
+    background-color: ${({ backgroundColor }) => backgroundColor};
+`;
 
-
-Section.Title = styled(({className, text})=>{
-    return (
-        <header className={className}>
-            <Zoom><h1>{text}</h1></Zoom>
-        </header>
-    )
-})`
+Section.Title = styled(({ className, text }) => (
+  <header className={className}>
+    <Zoom><h1>{text}</h1></Zoom>
+  </header>
+))`
    
-    color: ${({theme}) => theme.colors.primario};
-    text-align: center;
-    font-size: 2em;
+  color: ${({ theme }) => theme.colors.primario};
+  text-align: center;
+  font-size: 2em;
   padding-top: 7rem;
+`;
 
-Section.Body = styled(({className, children})=>{
-    return (
-        <div className={className}>
-            {children}
-        </div>
-    )
-})`
-width: 100%;
-
-  
-`
+Section.Body = styled(({ className, children }) => (
+  <div className={className}>
+    {children}
+  </div>
+))`
+  width: 100%;
+`;
 export default Section;
-
-
-
-
