48b2a813d5b508992a620d177d8bf4132948bfd4
Mathias Moreira <mathias.moreira@modelit.xyz>
2022-07-11 23:30:22 -0300

    Updates

---
+++ precache-manifest.fa23e1247030ecf56bfe48fe972a0e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb53abd709bd68adeadf7615d064b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd4c9f67126432a1bb5",
+    "url": "/static/css/main.ade023e1.chunk.css"
+  },
+  {
+    "revision": "3d0584bcc1753ad30637",
+    "url": "/static/js/2.23a73e8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.23a73e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd4c9f67126432a1bb5",
+    "url": "/static/js/main.d273da0e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.60a3021c8261b8345f4c13ea6e447b9a.js"
+  "/precache-manifest.fa23e1247030ecf56bfe48fe972a0e24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
